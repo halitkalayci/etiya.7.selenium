50e89c0d01025203f6a5c0981609a0e6737022d3
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-25 17:15:30 +0300

    test case invalid

--- test_login.py
+++ test_login.py
@@ -14,9 +14,19 @@ class TestLogin():
         password_input.send_keys("secret_sauce")
         login_button = driver.find_element(By.ID, "login-button")
         login_button.click()
-        assert True
+        assert driver.current_url == "https://www.saucedemo.com/inventory.html"
     def test_with_invalid_credentials(self):
-        assert False
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com/")
+        driver.maximize_window()
+        username_input = driver.find_element(By.ID, "user-name")
+        username_input.send_keys("error")
+        password_input = driver.find_element(By.ID,"password")
+        password_input.send_keys("secret_sauce")
+        login_button = driver.find_element(By.ID, "login-button")
+        login_button.click()
+        errorContainer = driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorContainer.text == "Epic sadface: Username and password do not match any user in this service"
 
 # configure
 # pytest
